de15ca222188b1ad92928bc8784ff6b80e66ed09
L <cuizhiwei@frontnetwork.com>
2014-08-19 15:38:10 +0800

    Initial Commit

---
+++ CLSideSlideController/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  CLSideSlideController
+//
+//  Created by L on 14-8-19.
+//  Copyright (c) 2014年 Cui. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
